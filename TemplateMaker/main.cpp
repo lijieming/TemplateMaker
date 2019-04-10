b07c8c52312387ecf47da61fbce49de3a0414203
Jeremy Lee <lijieming@hotmail.com>
2019-04-09 23:31:24 -0400

    Initial Commit

---
+++ TemplateMaker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TemplateMaker
+//
+//  Created by Jeremy Lee on 2019-04-09.
+//  Copyright © 2019 Jeremy Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
